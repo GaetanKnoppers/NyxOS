00bf2ee2db119c59c4493b7ff484379f4cef460f
GaetanKnoppers <knoppersgaetan@gmail.com>
2026-08-25 02:26:34 +0200

    Ajout du logo de boot

--- src/boot.asm
+++ src/boot.asm
@@ -17,11 +17,11 @@ header_end:
 
 section .rodata
 logo:
-    db " __   __     __  __     __  __     ______     ______", 0    
-    db "/\ "-.\ \   /\ \_\ \   /\_\_\_\   /\  __ \   /\  ___\", 0   
-    db "\ \ \-.  \  \ \____ \  \/_/\_\/_  \ \ \/\ \  \ \___  \", 0  
-    db " \ \_\\"\_\  \/\_____\   /\_\/\_\  \ \_____\  \/\_____\", 0 
-    db "  \/_/ \/_/   \/_____/   \/_/\/_/   \/_____/   \/_____/ ", 0
+    db ' __   __     __  __     __  __     ______     ______', 10    
+    db '/\ "-.\ \   /\ \_\ \   /\_\_\_\   /\  __ \   /\  ___\', 10   
+    db '\ \ \-.  \  \ \____ \  \/_/\_\/_  \ \ \/\ \  \ \___  \', 10  
+    db ' \ \_\\"\_\  \/\_____\   /\_\/\_\  \ \_____\  \/\_____\', 10 
+    db '  \/_/ \/_/   \/_____/   \/_/\/_/   \/_____/   \/_____/ ', 0
                                                         
 section .bss
 align 16
@@ -41,19 +41,45 @@ _start:
 
     mov esp, stack_top
 
-    mov word [0xb8000 + 160 + 0], 0x0F48
-    mov word [0xb8000 + 160 + 2], 0x0F65
-    mov word [0xb8000 + 160 + 4], 0x0F6C
-    mov word [0xb8000 + 160 + 6], 0x0F6C
-    mov word [0xb8000 + 160 + 8], 0x0F6F
-    mov word [0xb8000 + 160 + 10], 0x0F2C
-    mov word [0xb8000 + 160 + 12], 0x0F20
-    mov word [0xb8000 + 160 + 14], 0x0F57
-    mov word [0xb8000 + 160 + 16], 0x0F6F
-    mov word [0xb8000 + 160 + 18], 0x0F72
-    mov word [0xb8000 + 160 + 20], 0x0F6C
-    mov word [0xb8000 + 160 + 22], 0x0F64
+    mov esi, logo
+    mov edi, 0xB8000
+    call print_string
 
 .loop:
     hlt
     jmp .loop
+
+print_string:
+    ; ESI = texte
+    ; EDI = position VGA de départ
+
+    mov ebp, edi            ; position de début de la ligne
+
+.next:
+    lodsb
+
+    cmp al, 0
+    je .done
+
+    cmp al, 10
+    je .newline
+
+    mov ah, 0x0F
+    mov word [edi], ax
+    add edi, 2
+
+    jmp .next
+
+.newline:
+    add ebp, 160            ; ligne suivante
+    mov edi, ebp            ; retour à la colonne de départ
+    jmp .next
+
+.done:
+    ret
+clear_screen:
+    mov edi, 0xB8000
+    mov ecx, 80 * 25
+    mov ax, 0x0F20
+    rep stosw
+    ret
