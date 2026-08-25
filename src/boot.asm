26b2404afbf07403fadff45cc35151fce10f6800
GaetanKnoppers <knoppersgaetan@gmail.com>
2026-08-25 23:31:51 +0200

    Ajout du kernel en C

--- src/boot.asm
+++ src/boot.asm
@@ -14,14 +14,6 @@ header_start:
     dd 8
 
 header_end:
-
-section .rodata
-logo:
-    db ' __   __     __  __     __  __     ______     ______', 10    
-    db '/\ "-.\ \   /\ \_\ \   /\_\_\_\   /\  __ \   /\  ___\', 10   
-    db '\ \ \-.  \  \ \____ \  \/_/\_\/_  \ \ \/\ \  \ \___  \', 10  
-    db ' \ \_\\"\_\  \/\_____\   /\_\/\_\  \ \_____\  \/\_____\', 10 
-    db '  \/_/ \/_/   \/_____/   \/_/\/_/   \/_____/   \/_____/ ', 0
                                                         
 section .bss
 align 16
@@ -30,53 +22,20 @@ stack_bottom:
     resb 16384
 stack_top:
 
+
 section .text
 bits 32
 
 global _start
-
+extern kernel_main
 
 _start:
     cli
 
     mov esp, stack_top
 
-    mov esi, logo
-    mov edi, 0xB8000
-    call print_string
+    call kernel_main
 
-.loop:
+.hang:
     hlt
-    jmp .loop
-
-print_string:
-    mov ebp, edi
-    
-.next:
-    lodsb
-
-    cmp al, 0
-    je .done
-
-    cmp al, 10
-    je .newline
-
-    mov ah, 0x0F
-    mov word [edi], ax
-    add edi, 2
-
-    jmp .next
-
-.newline:
-    add ebp, 160            ; ligne suivante
-    mov edi, ebp            ; retour à la colonne de départ
-    jmp .next
-
-.done:
-    ret
-clear_screen:
-    mov edi, 0xB8000
-    mov ecx, 80 * 25
-    mov ax, 0x0F20
-    rep stosw
-    ret
+    jmp .hang
